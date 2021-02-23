d6a125bbf6948074604025bb82b85ac8bfdb06e3
Elizabeth Rivera <elizabethriver@gmail.com>
2021-02-22 19:38:21 -0500

    cambios

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,59 @@
 import { Component } from '@angular/core';
-
+import {Product} from './product.model'
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'store-angular';
+  title = 'Store - app angular';
+  items = ['nicolas', 'julian', 'andres'];
+  products: Product[] =[    {
+    id: '1',
+    image: 'assets/images/camiseta.png',
+    title: 'Camiseta',
+    price: 80000,
+    description: 'bla bla bla bla bla'
+  },
+  {
+    id: '2',
+    image: 'assets/images/hoodie.png',
+    title: 'Hoodie',
+    price: 80000,
+    description: 'bla bla bla bla bla'
+  },
+  {
+    id: '3',
+    image: 'assets/images/mug.png',
+    title: 'Mug',
+    price: 80000,
+    description: 'bla bla bla bla bla'
+  },
+  {
+    id: '4',
+    image: 'assets/images/pin.png',
+    title: 'Pin',
+    price: 80000,
+    description: 'bla bla bla bla bla'
+  },
+  {
+    id: '5',
+    image: 'assets/images/stickers1.png',
+    title: 'Stickers',
+    price: 80000,
+    description: 'bla bla bla bla bla'
+  },
+  {
+    id: '6',
+    image: 'assets/images/stickers2.png',
+    title: 'Stickers',
+    price: 80000,
+    description: 'bla bla bla bla bla'
+  }];
+  addItem = () => {
+    this.items.push('new item');
+  };
+  removeItem(index: number) {
+    this.items.splice(index, 1);
+  }
 }
