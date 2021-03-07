5bf67137314f6ddb16475b5816b9e9536b7f38f2
Elizabeth Rivera <elizabethriver@gmail.com>
2021-03-06 23:38:05 -0500

    cambios pipe total

--- src/app/shared/pipes/cart/cart.pipe.ts
+++ src/app/shared/pipes/cart/cart.pipe.ts
@@ -5,8 +5,18 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class CartPipe implements PipeTransform {
 
-  transform(value: unknown, ...args: unknown[]): unknown {
-    return null;
+  transform(objects: object[], id: string): any {
+    const countedObjects: object[] = [];
+
+    for (const object of objects) {
+      const count: any = objects.filter(obj => obj[id] === object[id]).length;
+      const x: any = countedObjects.find(obj => obj[id] === object[id]);
+      if (!x ) {
+        countedObjects.push({...object, count});
+      }
+    }
+
+    return countedObjects;
   }
 
 }
